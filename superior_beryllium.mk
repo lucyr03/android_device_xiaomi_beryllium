061ecea7c6d319bf1f3071a5ec5db5764e2ef0d0
Lucy <lucy_r03@yahoo.com>
2020-04-26 10:42:06 +0300

    beryllium: Superior Bringup
    Signed-off-by: Lucy <lucy_r03@yahoo.com>

--- superior_beryllium.mk
+++ superior_beryllium.mk
@@ -6,11 +6,13 @@
 
 $(call inherit-product, device/xiaomi/beryllium/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Superior OS stuff.
+$(call inherit-product, vendor/superior/config/common.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+DEVICE_MAINTAINERS="Lucy"
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_beryllium
+PRODUCT_NAME := superior_beryllium
 PRODUCT_DEVICE := beryllium
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO F1
